0bcbeb76bc7252cb0169f53de65f4bacc9d75b17
aakash2809 <aakashrajak289@gmail.com>
2020-12-05 02:01:19 +0530

    Added : Find maximum from three string values and test cases

--- src/main/java/com/FindMaximumUsingGenerics/FindMaximum.java
+++ src/main/java/com/FindMaximumUsingGenerics/FindMaximum.java
@@ -18,4 +18,14 @@ public class FindMaximum {
 			max = third;
 		return max;
 	}
+
+	public String findMax(String first, String second, String third) {
+		String max = first;
+		if (second.compareTo(max) > 0)
+			max = second;
+		if (third.compareTo(max) > 0)
+			max = third;
+		return max;
+	}
+
 }
--- src/test/java/com/FindMaximumUsingGenerics/FindMaximumTest.java
+++ src/test/java/com/FindMaximumUsingGenerics/FindMaximumTest.java
@@ -25,6 +25,7 @@ public class FindMaximumTest {
 		Integer result = findMax.findMax(5, 3, 9);
 		Assert.assertSame(9, result);
 	}
+
 	@Test
 	public void givenFloatMaxNumberAtFirstPostion_ReturnMaximumValue() {
 		FindMaximum findMax = new FindMaximum();
@@ -32,19 +33,41 @@ public class FindMaximumTest {
 		System.out.println(result);
 		Assert.assertEquals(9.0, result);
 	}
-	
+
 	@Test
 	public void givenFloatMaxNumberAtSeondPostion_ReturnMaximumValue() {
 		FindMaximum findMax = new FindMaximum();
 		float result = findMax.findMax(3.0f, 9.0f, 5.0f);
 		Assert.assertEquals(9.0, result);
 	}
-	
+
 	@Test
 	public void givenFloatMaxNumberAtThirdPostion_ReturnMaximumValue() {
 		FindMaximum findMax = new FindMaximum();
 		Float result = findMax.findMax(5.0f, 3.0f, 9.0f);
 		Assert.assertEquals(9.0, result);
+	}
+
+	@Test
+	public void givenStirngMaxNumberAtFirstPostion_ReturnMaximumValue() {
+		FindMaximum findMax = new FindMaximum();
+		String result = findMax.findMax("Peach", "Apple", "Banana");
+		System.out.println(result);
+		Assert.assertEquals("Peach", result);
+	}
+
+	@Test
+	public void givenStringMaxNumberAtSeondPostion_ReturnMaximumValue() {
+		FindMaximum findMax = new FindMaximum();
+		String result = findMax.findMax("Apple", "Peach", "Banana");
+		Assert.assertEquals("Peach", result);
+	}
+
+	@Test
+	public void givenStringMaxNumberAtThirdPostion_ReturnMaximumValue() {
+		FindMaximum findMax = new FindMaximum();
+		String result = findMax.findMax("Apple", "Banana", "Peach");
+		Assert.assertEquals("Peach", result);
 	}
 
 }
